a6a50c183aa38f307e6186b024697d4c5bd8d6e6
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 19:04:44 +0800

    Created program string formatting

--- batch_6/format_initial_letter_of _word_without_using_title().py
+++ batch_6/format_initial_letter_of _word_without_using_title().py
@@ -1,5 +1,12 @@
 # Ask for user input
+text = input("Enter a phrase: ")
+
 # Format first letter of each word to uppercase and the rest to lowercase
-# Split the string into words
+words = text.split() # Split the string into words
+capitalized_words = [word[0].upper() + word[1:].lower() if word else '' for word in words]
+
 # Join the words back 
+output = " ".join(capitalized_words)
+
 # Print result
+print("Output:", output)
