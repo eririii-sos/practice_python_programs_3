21d9cea48cc6aeef1446cbc4770a4019237e9215
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 23:25:47 +0800

    Created program string formatting

--- batch_6/capitalized_initials_without_using_capitalize().py
+++ batch_6/capitalized_initials_without_using_capitalize().py
@@ -1,3 +1,11 @@
 # Ask for user input
+text = input("Enter text: ")
+
 # Convert first letter to uppercase and the rest to lowercase manually
+if text:
+    capitalized_text = text[0].upper() + text[1:].lower()
+else:
+    capitalized_text = text
+
 # Print result
+print("Output:", capitalized_text)
