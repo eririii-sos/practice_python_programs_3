55303a54103caede61a4fce9d3f5fa62dba023c9
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 23:10:39 +0800

    Created program string formatting

--- batch_6/remove_leading_characters_at_the_beginning_matching_the_function_parameter.py
+++ batch_6/remove_leading_characters_at_the_beginning_matching_the_function_parameter.py
@@ -1,3 +1,10 @@
 # Ask for user input
+text = input("Enter a word: ")
+prefix = input("Enter the prefix to remove: ")
+
 # Check if the input starts with entered prefix and remove it manually
+if text.startswith(prefix):
+    text = text[len(prefix):]
+
 # Print result
+print("Output:", text)
