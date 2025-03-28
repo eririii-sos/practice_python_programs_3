c6104a7a1112f586011bf6dcaae0c916fc6b0cd2
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 22:36:52 +0800

    Created program string formatting

--- batch_6/check_if_characters_are_uppercase_without_using_isupper().py
+++ batch_6/check_if_characters_are_uppercase_without_using_isupper().py
@@ -5,4 +5,4 @@ text = input("Enter a phrase: ")
 upper_case = all(character >= 'A' and character <= 'Z' or not character.isalpha() for character in text)
 
 # Print result
-print("Are all the characters in uppercase?", upper_case)
+print("Are all the characters in uppercase?": upper_case)
