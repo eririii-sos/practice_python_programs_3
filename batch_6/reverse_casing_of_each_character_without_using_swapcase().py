ccd9cfbe2fde4e313e03811737ea58f0ebc6709f
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 19:56:57 +0800

    Created program string formatting

--- batch_6/reverse_casing_of_each_character_without_using_swapcase().py
+++ batch_6/reverse_casing_of_each_character_without_using_swapcase().py
@@ -1,3 +1,8 @@
 # Ask for user input
+text = input("Enter a phrase: ")
+
 # Mimic swap case functionality using loop and conditional demands
-# Print result
+swapped_text = "".join([character.upper() if character.islower() else character.lower() for character in text])
+
+# Print result 
+print("Output:", swapped_text)
