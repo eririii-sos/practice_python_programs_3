2169fefdee08a34c9048b04f98e149c8034c0ab0
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 22:55:12 +0800

    Created program string formatting

--- batch_6/convert_all_characters_to_lowercase_without_using_lower().py
+++ batch_6/convert_all_characters_to_lowercase_without_using_lower().py
@@ -1,3 +1,8 @@
 # Ask for user input
+text = input("Enter a phrase: ")
+
 # Format all uppercase characters to lowercase
+lower_case = "".join(chr(ord(character) + 32) if 'A' <= character <= 'Z' else character for character in text)
+
 # Print result
+print("Output:", lower_case)
