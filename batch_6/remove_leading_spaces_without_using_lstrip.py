258095156d3fe72d47fe0ae8e9350403c22262a7
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 15:12:38 +0800

    Fixed misspelling

--- batch_6/remove_leading_spaces_without_using_lstrip.py
+++ batch_6/remove_leading_spaces_without_using_lstrip.py
@@ -6,7 +6,7 @@ index = 0
 while index < len(full_name) and full_name[index] == " ":
     index += 1
 
-formatter_name = full_name[index:] # Starts from the non-space character
+formatted_name = full_name[index:] # Starts from the non-space character
 
 # Print result
-print("Output:", formatter_name)
+print("Output:", formatted_name)
