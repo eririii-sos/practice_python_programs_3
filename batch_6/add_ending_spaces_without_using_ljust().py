769660df06f0889384752c19afe463c8c70e01bb
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 23:50:19 +0800

    Created program string formatting

--- batch_6/add_ending_spaces_without_using_ljust().py
+++ batch_6/add_ending_spaces_without_using_ljust().py
@@ -1,3 +1,9 @@
 # Ask for user input
+text = input("Enter text: ")
+width = int(input("Enter total width: "))
+
 # Add spaces at the end to match the total width
+formatted_text = text + " " * (width - len(text)) if len(text) < width else text
+
 # Print result
+print(f"Output:|{formatted_text}|")
