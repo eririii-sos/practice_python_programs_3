f2a966bf0de9537a24da1d72810a1eded3ae7a12
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 23:37:41 +0800

    Created program string formatting

--- batch_6/check_end_characters_if_it_matches_function_parameter.py
+++ batch_6/check_end_characters_if_it_matches_function_parameter.py
@@ -1,3 +1,9 @@
 # Ask for user input
+text = input("Enter text: ")
+suffix = input("Enter the suffix to check: ")
+
 # Check if the string ends with the given suffix manually
+check_suffix = text[-len(suffix):] == suffix if len(suffix) <= len(text) else False
+
 # Print result
+print("Does the text end with the entered suffix?", check_suffix)
