7341dcab510615b4f84aa917cd6a7e40ab9e979d
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-28 15:09:37 +0800

    Created program string formatting

--- batch_6/remove_leading_spaces_without_using_lstrip.py
+++ batch_6/remove_leading_spaces_without_using_lstrip.py
@@ -1,3 +1,12 @@
 # Ask for user input
+full_name = input("Enter your full name beginning with space(s): ")
+
 # Remove the spaces
+index = 0
+while index < len(full_name) and full_name[index] == " ":
+    index += 1
+
+formatter_name = full_name[index:] # Starts from the non-space character
+
 # Print result
+print("Output:", formatter_name)
