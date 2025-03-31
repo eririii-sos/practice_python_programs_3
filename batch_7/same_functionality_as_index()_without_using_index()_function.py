e506c6d7c25947658e0dbe45f396ce4531665b36
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-04-01 01:05:19 +0800

    Created program string formatting

--- batch_7/same_functionality_as_index()_without_using_index()_function.py
+++ batch_7/same_functionality_as_index()_without_using_index()_function.py
@@ -1,3 +1,13 @@
 # Ask for user input
+text = input("Enter text: ")
+substring = input("Enter the substring to find: ")
+
 # Find the first occurrence of the substring manually
-# Print result 
+index = -1
+for i in range(len(text) - len(substring) + 1):
+    if text[i:i+len(substring)] == substring:
+        index = i
+        break
+
+# Print result
+print(f"The substring first appears at index {index}.") 
