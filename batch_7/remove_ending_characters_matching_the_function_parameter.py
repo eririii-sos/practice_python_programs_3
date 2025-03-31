c76d2c129b53b7411228e7900e163e8ec66ffe5c
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-31 23:24:46 +0800

    Created program string formatting

--- batch_7/remove_ending_characters_matching_the_function_parameter.py
+++ batch_7/remove_ending_characters_matching_the_function_parameter.py
@@ -1,3 +1,10 @@
 # Ask for user input
+word = input("Enter a word: ")
+suffix = input("Enter the suffix to remove: ")
+
 # Check if the input ends with entered suffix and remove it manually
+if word.endswith(suffix):
+    word = word[:len(word) - len(suffix)]
+
 # Print result
+print("Output:", word)
