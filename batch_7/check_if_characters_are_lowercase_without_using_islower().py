187aaabe7c521308ff89d7edeab8d6c4ce50b5c8
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-31 23:53:22 +0800

    Created program string formatting

--- batch_7/check_if_characters_are_lowercase_without_using_islower().py
+++ batch_7/check_if_characters_are_lowercase_without_using_islower().py
@@ -1,3 +1,8 @@
 # Ask user for input
+text = input("Enter text: ")
+
 # Check if all characters are lowercase manually
+lower_case = all('a' <= character <= 'z' or not character.isalpha() for character in text)
+
 # Print result
+print("Are all the characters in lowercase?", lower_case)
