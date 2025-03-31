6097392494112f5112a56e591a9f5861bb407322
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-04-01 01:20:08 +0800

    Created program string formatting

--- batch_7/same_functionality_as_rindex()_without_using_rindex()_function.py
+++ batch_7/same_functionality_as_rindex()_without_using_rindex()_function.py
@@ -1,3 +1,12 @@
 # Ask for user input
+text = input("Enter text: ")
+substring = input("Enter the substring to find: ")
+
 # Find the last occurrence of the substring manually
+index = -1
+for i in range(len(text) - len(substring), -1, -1):
+    if text[i:i+len(substring)] == substring:
+        index = 1
+        break
+
 # Print result
