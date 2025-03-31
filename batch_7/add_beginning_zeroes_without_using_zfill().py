7f3dc3736cd224666e3e17c5f761b651ab575054
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-04-01 00:38:31 +0800

    Created program string formatting

--- batch_7/add_beginning_zeroes_without_using_zfill().py
+++ batch_7/add_beginning_zeroes_without_using_zfill().py
@@ -1,3 +1,9 @@
 # Ask for user input
+text = input("Enter text: ")
+width = int(input("Enter total width: "))
+
 # Add zeroes at the beginning to match the total width
+formatted_text = "0" * (width - len(text)) + text if len(text) < width else text
+
 # Print result
+print("Output:", formatted_text)
