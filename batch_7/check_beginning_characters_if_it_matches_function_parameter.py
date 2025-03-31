353cb132efc8032ac5506b419843312aa99fdca3
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-04-01 00:09:53 +0800

    Created program string formatting

--- batch_7/check_beginning_characters_if_it_matches_function_parameter.py
+++ batch_7/check_beginning_characters_if_it_matches_function_parameter.py
@@ -1,3 +1,9 @@
 # Ask for user input
+text = input("Enter text: ")
+prefix = input("Enter the prefix to check: ")
+
 # Check if the string starts with the given prefix manually
+check_prefix = text[:len(prefix)] == prefix if len(prefix) <= len(text) else False
+
 # Print result
+print("Does the text start with the entered prefix?", check_prefix)
