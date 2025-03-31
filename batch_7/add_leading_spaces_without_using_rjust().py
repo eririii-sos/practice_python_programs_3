fcaacee2db54fd4a916fe9a61cad9565f97e8831
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-04-01 00:22:23 +0800

    Created program string formatting

--- batch_7/add_leading_spaces_without_using_rjust().py
+++ batch_7/add_leading_spaces_without_using_rjust().py
@@ -1,3 +1,9 @@
 # Ask for user input
+text = input("Enter text: ")
+width = int(input("Enter total width: "))
+
 # Add spaces at the beginning to match the total width
+formatted_text = " " * (width - len(text)) + text if len(text) < width else text
+
 # Print result
+print("Output:", formatted_text)
