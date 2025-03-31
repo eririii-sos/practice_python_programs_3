cf42b416508aa6dd816798aa25b2069c04868b7c
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-31 23:41:46 +0800

    Created program string formatting

--- batch_7/convert_all_characters_to_uppercase_without_using_upper().py
+++ batch_7/convert_all_characters_to_uppercase_without_using_upper().py
@@ -1,3 +1,8 @@
 # Ask for user input
+text = input("Enter text: ")
+
 # Convert all lowercase characters to uppercase
+upper_case = "".join(chr(ord(character) - 32 ) if 'a' <= character <= 'z' else character for character in text)
+
 # Print result
+print("Output:", upper_case)
