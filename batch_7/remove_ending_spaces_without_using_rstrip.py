958ccbd3b49b08178eb16815e67d54c9ac10c601
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-31 23:00:30 +0800

    Created program string formatting

--- batch_7/remove_ending_spaces_without_using_rstrip.py
+++ batch_7/remove_ending_spaces_without_using_rstrip.py
@@ -1,3 +1,12 @@
 # Ask for user input
+text = input("Enter text with trailing spaces: ")
+
 # Remove the ending spaces
+index = len(text) - 1
+while index >=0 and text[index] == " ":
+    index -= 1
+
+formatted_text = text[:index + 1] # Keep all characters except the ending non-space characters
+
 # Print result
+print("Output:", formatted_text)
